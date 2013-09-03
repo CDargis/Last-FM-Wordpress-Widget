21616772af3a02f9c9c3d28b31a5a3b28d7fbc5b
CDargis <dargis.chris@gmail.com>
2013-09-02 21:33:20 -0500

    Updating spacing

--- lastFM.php
+++ lastFM.php
@@ -11,68 +11,68 @@ License: GPL2
 */
 
 add_action('widgets_init', function() {
-	register_widget('Last_FM_Widget');
+  register_widget('Last_FM_Widget');
 });
 
 class Last_FM_Widget extends WP_Widget {
-	
-	function Last_FM_Widget() {
-		parent::WP_Widget(false, $name = __('Last FM Song History', 'wp_widget_plugin'));
-	}
+  
+  function Last_FM_Widget() {
+    parent::WP_Widget(false, $name = __('Last FM Song History', 'wp_widget_plugin'));
+  }
 
-	// widget form creation
-	function form($instance) {
+  // widget form creation
+  function form($instance) {
 
-		// Check values
-		if($instance) {
-	   $title = esc_attr($instance['title']);
-	   $userName = esc_attr($instance['userName']);
-		} else {
-	   $title = '';
-	   $userName = '';
-	}
-	?>
+    // Check values
+    if($instance) {
+     $title = esc_attr($instance['title']);
+     $userName = esc_attr($instance['userName']);
+    } else {
+     $title = '';
+     $userName = '';
+  }
+  ?>
 
-	<p>
-	<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'wp_widget_plugin'); ?></label>
-	<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
-	</p>
+  <p>
+  <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'wp_widget_plugin'); ?></label>
+  <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+  </p>
 
-	<p>
-	<label for="<?php echo $this->get_field_id('userName'); ?>"><?php _e('Last FM User Name:', 'wp_widget_plugin'); ?></label>
-	<input class="widefat" id="<?php echo $this->get_field_id('userName'); ?>" name="<?php echo $this->get_field_name('userName'); ?>" type="text" value="<?php echo $userName; ?>" />
-	</p>
+  <p>
+  <label for="<?php echo $this->get_field_id('userName'); ?>"><?php _e('Last FM User Name:', 'wp_widget_plugin'); ?></label>
+  <input class="widefat" id="<?php echo $this->get_field_id('userName'); ?>" name="<?php echo $this->get_field_name('userName'); ?>" type="text" value="<?php echo $userName; ?>" />
+  </p>
 
-	<?php
-	}
+  <?php
+  }
 
-	// update widget
-	function update($new, $old) {
-		$instance = $old;
-		// Fields
-		$instance['title'] = strip_tags($new['title']);
-		$instance['userName'] = strip_tags($new['userName']);
-		return $instance;
-	}
+  // update widget
+  function update($new, $old) {
+    $instance = $old;
+    // Fields
+    $instance['title'] = strip_tags($new['title']);
+    $instance['userName'] = strip_tags($new['userName']);
+    return $instance;
+  }
 
-	// display widget
-	function widget($args, $instance) {
-		extract( $args );
-		// these are the widget options
-		$title = apply_filters('widget_title', $instance['title']);
-		$userName = $instance['userName'];
-		echo $before_widget;
-		// Display the widget
-		echo '<div class="widget-text wp_widget_plugin_box">';
+  // display widget
+  function widget($args, $instance) {
+    extract( $args );
+    // these are the widget options
+    $title = apply_filters('widget_title', $instance['title']);
+    $userName = $instance['userName'];
+    echo $before_widget;
+    // Display the widget
+    echo '<div class="widget-text wp_widget_plugin_box">';
 
    // Check if title is set
    if ( $title ) {
-   	echo $before_title . $title . $after_title;
+    echo $before_title . $title . $after_title;
    }
 
    // Check if text is set
    if( $userName ) {
-   	$numTracks = 5;
+    $numTracks = 5;
     echo '<h5 class="wp_widget_plugin_text last-fm-widget-header">'.'Last '.$numTracks.' tracks for '.$userName.':</h5>';
     
     $baseURI = 'https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&format=json&api_key=fd04949ecca5f5d53a875f057fce53e6';
@@ -82,15 +82,15 @@ class Last_FM_Widget extends WP_Widget {
 
     // Iterate through each song:
     for($i = 0; $i < $numTracks; $i++) {
-    	$track = $obj['recenttracks']['track'][$i]['name'];
-    	$artist = $obj['recenttracks']['track'][$i]['artist']['#text'];
-    	echo '<p>'.$track.' - '.$artist.'</p>';
+      $track = $obj['recenttracks']['track'][$i]['name'];
+      $artist = $obj['recenttracks']['track'][$i]['artist']['#text'];
+      echo '<p>'.$track.' - '.$artist.'</p>';
     }
    }
 
    echo '</div>';
    echo $after_widget;
-	}
+  }
 }
 
 ?>
